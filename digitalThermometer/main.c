e7aaacbefe55cde92ca4c180b6ffa199ce7ca411
Ledrunning <ledrunner@hotmail.com>
2020-01-27 21:43:29 +0300

    have a bug on negative temperature

--- digitalThermometer/main.c
+++ digitalThermometer/main.c
@@ -19,10 +19,14 @@
 #define DHT_PIN         PIND
 #define DHT22_PIN 6
 #define STRING_SIZE 3
+#define DEC 10
+#define DOZEN 10
 
-uint8_t c=0,lowByteRh, highByteRh, lowByteTemp, highByteTemp, checkSum;
+uint8_t data=0,lowByteRh, highByteRh, lowByteTemp, highByteTemp, checkSum;
 uint16_t temperatureResult, humidityResult;
 
+void print_humidity(char* buffer);
+void print_temperature(char* buffer, uint16_t temp_after_point);
 void request();                /* Microcontroller send start pulse/request */
 void response();				/* receive response from DHT11 */
 uint8_t receive_data();			/* receive data */
@@ -31,7 +35,7 @@ int get_checksum();
 int main(void) {
 	
 	char buffer[STRING_SIZE];
-	int temp_buffer;
+	uint16_t temp_buffer;
     uint16_t temp_buffer_after_point;
 
 	lcdInit();
@@ -48,43 +52,50 @@ int main(void) {
 		lowByteTemp=receive_data();	/* store next eight bit in I_Temp */
 		highByteTemp=receive_data();	/* store next eight bit in D_Temp */
 		checkSum=receive_data();/* store next eight bit in CheckSum */
+		print_humidity(buffer);
+
 		
 		if (get_checksum() != checkSum)
 		{
-			lcdGotoXY(0,1);
-			lcdPuts("Error");
+			lcdGotoXY(0,0);
+			lcdPuts("Sens    ");
+			lcdGotoXY(1,0);
+			lcdPuts("Err     ");
 		}
 		else
 		{
-			lcdClear();
-			lcdGotoXY(0,0);
-			lcdPuts("H=");
-			lcdGotoXY(0,3);
-			humidityResult = (lowByteRh * 256 + highByteRh ) / 10;
-			itoa(humidityResult, buffer, 10);
-			lcdPuts(buffer);
-			
-			lcdGotoXY(1,0);
-			lcdPuts("T=");
-			lcdGotoXY(1,3);
+			humidityResult = (lowByteRh * 256 + highByteRh ) / DOZEN;
+			print_humidity(buffer);
 			
 			temperatureResult = (lowByteTemp * 256 + highByteTemp );
 			temp_buffer = temperatureResult;
-						
-			itoa(temperatureResult, buffer, 10);
-			lcdPuts(buffer);
-			lcdGotoXY(1,5);
-			lcdPuts(".");
-			lcdGotoXY(1,6);
-			temp_buffer_after_point = temp_buffer % 10;
-			itoa(temp_buffer_after_point, buffer, 10);
-			lcdPuts(buffer);
-			lcdGotoXY(1,8);
-			lcdPuts("C");
-			
+			temp_buffer_after_point = temp_buffer % DOZEN;
+			print_temperature(buffer, temp_buffer_after_point);
 		}
 		_delay_ms(1000);
 	}
+}
+
+void print_humidity(char* buffer)
+{
+	lcdGotoXY(0,0);
+	lcdPuts("H=");
+	lcdGotoXY(0,2);
+	itoa(humidityResult, buffer, DOZEN);
+	lcdPuts(buffer);
+	lcdGotoXY(0, 5);
+	lcdPuts("%");
+}
+
+void print_temperature(char* buffer, uint16_t temp_after_point)
+{
+	lcdGotoXY(1,0);
+	lcdPuts("T=");
+	lcdGotoXY(1,2);
+	itoa(temperatureResult, buffer, DEC);
+	lcdPuts(buffer);
+	lcdGotoXY(1,6);
+	lcdPuts("C");
 }
 
 void request()                /* Microcontroller send start pulse/request */
@@ -95,7 +106,7 @@ void request()                /* Microcontroller send start pulse/request */
 	PORTD |= (1<<DHT22_PIN);	/* set to high pin */
 }
 
-void response()				/* receive response from DHT11 */
+void response()				/* receive response from DHT22 */
 {
 	DDRD &= ~(1<<DHT22_PIN);
 	while(PIND & (1<<DHT22_PIN));
@@ -105,18 +116,18 @@ void response()				/* receive response from DHT11 */
 
 uint8_t receive_data()			/* receive data */
 {
-	int q;
-	for (q=0; q<8; q++)
+	int i;
+	for (i=0; i<8; i++)
 	{
 		while((PIND & (1<<DHT22_PIN)) == 0);  /* check received bit 0 or 1 */
 		_delay_us(30);
 		if(PIND & (1<<DHT22_PIN))/* if high pulse is greater than 30ms */
-		c = (c<<1)|(0x01);	/* then its logic HIGH */
+		data = (data<<1)|(0x01);	/* then its logic HIGH */
 		else			/* otherwise its logic LOW */
-		c = (c<<1);
+		data = (data<<1);
 		while(PIND & (1<<DHT22_PIN));
 	}
-	return c;
+	return data;
 }
 
 int get_checksum(){
